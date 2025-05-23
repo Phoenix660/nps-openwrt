a90421cb6ce8f0c13e63e210aa55102ed1c9cce2
Phoenix660 <132873157+Phoenix660@users.noreply.github.com>
2025-05-23 09:12:16 +0800

    Update npc.lua

--- luci-app-npc/luasrc/controller/npc.lua
+++ luci-app-npc/luasrc/controller/npc.lua
@@ -1,17 +1,24 @@
-module("luci.controller.npc",package.seeall)
+module("luci.controller.npc", package.seeall)
 
 function index()
-	if not nixio.fs.access("/etc/config/npc") then
-		return
-	end
+    if not nixio.fs.access("/etc/config/npc") then
+        return
+    end
 
-	entry({"admin", "services", "npc"}, cbi("npc"), _("NPS Client"), 99).dependent = true
-	entry({"admin", "services", "npc", "status"}, call("act_status")).leaf = true
+    -- 检查 UCI 配置中的 hidden 选项
+    local uci = require "luci.model.uci".cursor()
+    local hidden = uci:get("npc", "settings", "hidden") == "1"
+
+    -- 如果未隐藏，则注册菜单项
+    if not hidden then
+        entry({"admin", "services", "npc"}, cbi("npc"), _("NPS Client"), 99).dependent = true
+        entry({"admin", "services", "npc", "status"}, call("act_status")).leaf = true
+    end
 end
 
 function act_status()
-	local e = {}
-	e.running = luci.sys.call("pgrep npc > /dev/null") == 0
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(e)
+    local e = {}
+    e.running = luci.sys.call("pgrep npc > /dev/null") == 0
+    luci.http.prepare_content("application/json")
+    luci.http.write_json(e)
 end
